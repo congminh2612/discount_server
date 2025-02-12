0faf004fd6bb4c173529819d8f11d2cac12d3b3b
zzmzz <zz@gmail.com>
2025-02-13 01:18:20 +0700

    build model with sequelize

--- src/models/user.js
+++ src/models/user.js
@@ -1,36 +1,42 @@
 // models/user.model.js
 import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
 
-export default (sequelize) => {
-  const User = sequelize.define(
-    'User',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+const User = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // phone: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+    role_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Role',
+        key: 'id',
       },
     },
-    {
-      tableName: 'user',
-      timestamps: true,
-    }
-  );
-
-  return User;
-};
+  },
+  {
+    tableName: 'User',
+    timestamps: true,
+    underscored: true,
+  }
+);
+export default User;
