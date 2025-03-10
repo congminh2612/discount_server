e7f33b0d0f38ce81b70896b518908fade441adb5
zzmzz <zz@gmail.com>
2025-03-11 01:04:04 +0700

    feature: sync user firebase with server

--- src/controller/user/user.controller.js
+++ src/controller/user/user.controller.js
@@ -9,6 +9,29 @@ const getUser = async (req, res) => {
   }
 };
 
+const syncUser = async (req, res) => {
+  try {
+    const { uid, email, username, phone, avatar } = req.user;
+
+    let user = await User.findOne({ where: { firebase_uid: uid } });
+
+    if (!user) {
+      user = await User.create({
+        firebase_uid: uid,
+        email,
+        name: username,
+        phone,
+        avatar,
+        role_id: 2,
+      });
+    }
+
+    return res.json({ user });
+  } catch (error) {
+    return res.status(500).json({ message: 'Lỗi đồng bộ user', error: error.message });
+  }
+};
+
 const createUser = async (req, res) => {
   res.status(200).json({ message: 'Create user' });
 };
@@ -19,4 +42,4 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {};
 
-export { getUser, createUser, updateUser, deleteUser };
+export { getUser, createUser, updateUser, deleteUser, syncUser };
